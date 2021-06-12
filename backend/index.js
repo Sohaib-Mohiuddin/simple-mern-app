1f8876ffb50b0262b2f4dbf28bc194debc8aea77
Sohaib-DigitalOcean-Ubuntu20 <sohaibm131@gmail.com>
2021-06-12 00:09:29 +0000

    Cleaning Backend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const host = '0.0.0.0';
-var request = require('request')
+require('dotenv/config');
+
+// MongoDB vars
+const mongoose = require('mongoose'); // required for MongoDB connect
+const mongoUrl = process.env.DB_URL; // MUST HAVE MONGODB URL IN ENVIRONMENT VARIABLE
+
+var request = require('request');
 
 app.get('/api', (req, res) => res.json({ message: 'Hello from simple-mern-app backend api' }));
 app.get('/api/randomimg', (req, res) => {
@@ -14,6 +20,15 @@ app.get('/api/randomimg', (req, res) => {
         }
     });
 });
+
+// MongoDB Connection
+mongoose.connect(
+    mongoUrl,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('Connected to DB')
+    }
+);
 
 app.listen(port, () => {
     console.log(`Connection to backend established. Running on ${host}:${port}`)
